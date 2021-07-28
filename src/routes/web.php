0acd37524355cf48cf806c9a73333bad4cac0e03
Neha0921 <neha.reddy@nitorinfotech.com>
2021-07-28 21:01:11 +0530

    Add version v1.1.3

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Neha0921\SubstrateApiPhp\Http\Controllers'],function(){
 
 
-    Route::get('SubstrateApi',"SubstrateApiController")->name('SubstrateApi');
+   /*  Route::get('SubstrateApi',"SubstrateApiController")->name('SubstrateApi');
     Route::get('rpc',"RpcController")->name('rpc');
-    Route::get('substrate-api',"SystemController")->name('system');
+    Route::get('substrate-api',"SystemController")->name('system'); */
 
     Route::get('rpcCall',"RpcController@index")->name('dashboard');
 
